b0764f64f410ec9aa4ea5cbc74765d7217086375
Irina Garmaeva <garmaeva-i@mail.ru>
2023-08-09 20:56:07 +0300

    delete server crash test message

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,11 +8,6 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const NotFoundError = require('../errors/notFoundError');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 router.use('/', loginRoute);
 router.use('/', signUpRoute);
 
